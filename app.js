2d6c60f0c4a968b8b906004685f8a208be0ebf85
jugo92 <jugzek92@outlook.fr>
2024-09-28 17:07:00 +0200

    correction prometheus

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const client = require('prom-client');
 
 const app = express();
 
+const register = new client.Registry();
+
+const temperatureGauge = new client.Gauge({
+  name: 'weather_temperature_celsius',
+  help: 'Température actuelle en degrés Celsius',
+});
+
+const humidityGauge = new client.Gauge({
+  name: 'weather_humidity_percent',
+  help: 'Humidité actuelle en pourcentage',
+});
+
+register.registerMetric(temperatureGauge);
+register.registerMetric(humidityGauge);
+
 app.get('/metrics', async (req, res) => {
-    // modifiez l'endpoint pour retourner les métriques demandées
-    return res.send("Retourne les métriques demandées");
+
+  temperatureGauge.set((Math.random() * 45) - 10);
+  humidityGauge.set(Math.random() * 100);
+
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 const PORT = 3000;
